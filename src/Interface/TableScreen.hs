66bedb96b247af67e3e749f54f6f6d9c24b3ab36
Artem Bondar <36895882+nh88dx@users.noreply.github.com>
2018-04-23 00:26:54 +0300

    Sample window render, center alignment on screen depending on resolution

--- src/Interface/TableScreen.hs
+++ src/Interface/TableScreen.hs
@@ -1,6 +1,44 @@
 module Interface.TableScreen where
 
-import Graphics.Gloss
+import Graphics.Gloss.Interface.Environment
+import Graphics.Gloss.Interface.Pure.Game
 
+-- | Launch main game screen.
 launchTableScreen :: IO ()
-launchTableScreen =  display (InWindow "Game window" (200, 200) (10, 10)) white (Circle 80)
+launchTableScreen =  do
+    tableScreen <- initTableScreen
+    resolution <- getScreenSize
+    play (display (screenPosition resolution)) bgColor fps tableScreen drawTableScreen handleTableScreen updateTableScreen
+    where
+      display = InWindow "Lambdem Poker" (screenSize)
+      bgColor = white   -- цвет фона
+      fps     = 30      -- кол-во кадров в секунду
+
+screenSize :: (Int, Int)
+screenSize = (960, 720)
+
+screenPosition :: (Int, Int) -> (Int, Int)
+screenPosition (w, h) 
+    | w == 1920 = (480, height)
+    | otherwise              = (10, height)
+    where
+      height
+        | h == 1080 = 180
+        | otherwise = 10
+
+
+data TableScreen = TableScreen
+  { var :: Int
+  }
+
+initTableScreen :: IO TableScreen
+initTableScreen = pure TableScreen {var = 1}
+
+drawTableScreen :: TableScreen -> Picture
+drawTableScreen _ = text "some text"
+
+handleTableScreen :: Event -> TableScreen -> TableScreen
+handleTableScreen _ = id
+
+updateTableScreen :: Float -> TableScreen -> TableScreen
+updateTableScreen _ = id
