23f2695823bc87957844ae0c201d3d68beb20126
Artem Bondar <36895882+nh88dx@users.noreply.github.com>
2018-05-07 21:14:47 +0300

    Added functions to operate with positions ordering

--- src/Poker/Logic/Trading.hs
+++ src/Poker/Logic/Trading.hs
@@ -3,21 +3,60 @@ module Poker.Logic.Trading where
 
 import Poker.Logic.Types
 
+-------------------------------------------------------------------------------
+-- * Position processing functions
+-------------------------------------------------------------------------------
+
 -- | Set first player to perform action depending on street.
 setFirstPlayer :: [Player] -> Street -> [Player]
-setFirstPlayer players street = setActivePlayer players firstPositon 
-    where
-        firstPositon = case length players of
-            2 -> if (street == Preflop)
-                    then BB
-                    else SB
-            _ -> UTG
+setFirstPlayer players street = 
+    toggleNewActivePlayer players (getFirstPosition (length players) street)
 
--- | Set active player depending to given position.
-setActivePlayer :: [Player] -> Position -> [Player]
-setActivePlayer players pos
+-- | Set active player depending to given position
+--   and unsets previous active player.
+toggleNewActivePlayer :: [Player] -> Position -> [Player]
+toggleNewActivePlayer [] _ = []
+toggleNewActivePlayer players pos
     | (position $ head players) == pos =
-        ((head players) { active = True } : tail players)
+        ((head players) { active = True } : toggleNewActivePlayer (tail players) pos)
     | otherwise =
-        (head players : setActivePlayer (tail players) pos)
-        
+        (player : toggleNewActivePlayer (tail players) pos)
+    where
+        player = case active $ head players of
+            True  -> player { active = False }
+            False -> player
+
+-- | Return type of active player.
+getActivePlayerType :: [Player] -> PlayerType
+getActivePlayerType players = case active $ head players of
+    True  -> control $ head players
+    False -> getActivePlayerType $ tail players
+
+-- | Return first position depending on amount of players and street.
+getFirstPosition :: Int -> Street -> Position
+getFirstPosition amountOfPlayers street = case amountOfPlayers of
+    2 -> if (street == Preflop)
+            then BB
+            else SB
+    _ -> UTG
+
+-- | Return next position depending on amount of players and street.
+getNextPositon :: Int -> Street -> Position -> Position
+getNextPositon amountOfPlayers street previousPosition =
+    case amountOfPlayers of
+        2 -> if (street == Preflop)
+                then SB
+                else BB
+        _ -> MP
+
+-- | Return last position depending on amount of players and street.
+getLastPosition :: Int -> Street -> Position
+getLastPosition amountOfPlayers street = case amountOfPlayers of
+    2 -> if (street == Preflop)
+            then SB
+            else BB
+    _ -> BTN
+
+-------------------------------------------------------------------------------
+-- * Players actions processing functions
+-------------------------------------------------------------------------------
