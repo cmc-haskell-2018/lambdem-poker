af39b37a512e1bc3174b34e251b994c975e41de9
Artem Bondar <36895882+nh88dx@users.noreply.github.com>
2018-05-13 19:04:22 +0300

    Declared some basic types for AI

--- src/Poker/AI/Types.hs
+++ src/Poker/AI/Types.hs
@@ -1,2 +1,35 @@
 -- | All AI related types are declared here.
 module Poker.AI.Types where
+
+import System.Random (StdGen)
+
+import Poker.Logic.Types
+
+-------------------------------------------------------------------------------
+-- * Declarations
+-------------------------------------------------------------------------------
+
+data AIPlayer = AIPlayer
+  { hand       :: [Card]    -- ^ dealt hand
+  , board      :: [Card]    -- ^ current board
+  , playStyle  :: PlayStyle -- ^ type of play style
+  , pfr        :: Bool      -- ^ preflop raise
+  , randomizer :: StdGen    -- ^ random number generator
+  }
+
+data PlayStyle = PlayStyle
+  { playStyleType :: PlayStyleType
+
+  }
+
+data PlayStyleType
+  = Telephone
+  | Passive
+  | Tight
+  | Aggresive
+  | Random
+
+-------------------------------------------------------------------------------
+-- * Constructors
+-------------------------------------------------------------------------------
+
